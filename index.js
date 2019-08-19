96b2a4570eba253a3da1904c18bfdd22fa1aff57
Pablo Sarabia <pablosarabia@Pablos-MacBook-Pro.local>
2019-08-19 14:08:42 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+let clients = [];
+
+let emitClients = () => {
+  io.emit('clients', clients);
+}
+
+io.serveClient('origins', 'http://www.pivotally.com:80 https://www.pivotally.com:443 http://pivotally.com:80 https://pivotally.com:443')
+
+io.on('connection', socket => {
+  emitClients();
+  socket.on('register', client => {
+      let pos = clients.length;
+      let notFound = true;
+      while(pos--) {
+        if (clients[pos].id === client.id) {
+          notFound = false;
+        }
+      }
+      if (notFound) {
+        clients.push(client);
+      }
+      socket.join(client.id);
+      emitClients();
+      
+  });
+  socket.on('signalling', data => {
+    console.log(data);
+    io.to(data.client.id).emit('signalling', data);
+  });
+
+  socket.on('clearRooms', () => {
+    
+    clients = [];
+    emitClients();
+  });
+
+  socket.on('disconnect', (client) => { 
+      //console.log('Disconnect', client);
+  });
+});
+
+(process.env.NODE_ENV === 'production') ? server.listen(80) : server.listen(3001);
